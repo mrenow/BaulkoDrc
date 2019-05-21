9f6b1787626c7d9b5febe38892fb28910f144f9a
Ezra <other.ezhui@gmail.com>
2019-05-21 15:02:17 +1000

    Drive code

---
+++ Modules/Maestro.py
@@ -0,0 +1,104 @@
+import serial
+from numpy import interp
+import time
+
+maestro = serial.Serial(port='COM17')
+
+previous_speed = 1
+
+
+def open_maestro():
+    maestro.open()
+
+
+def close_maestro():
+    maestro.close()
+
+
+class Channel:
+
+    def __init__ (self, channel):
+        self.channel = channel
+        self.min = 0
+        self.max = 0
+
+    def set_range(self, min, max):
+        self.min = min
+        self.max = max
+
+    def set_target(self, target):
+        if target < self.min:
+            target = self.min
+        if target > self.max:
+            target = self.max
+
+        lower = target & 0x7f
+        upper = (target >> 7) & 0x7f
+        command = chr(0x84) + chr(self.channel) + chr(lower) + chr(upper)
+
+        maestro.write(bytes(command, 'latin-1'))
+        print(bytes(command, 'latin-1'))
+
+    def set_speed(self, target):
+        lower = target & 0x7f
+        upper = (target >> 7) & 0x7f
+        command = chr(0x87) + chr(self.channel) + chr(lower) + chr(upper)
+
+        maestro.write(bytes(command, 'latin-1'))
+
+    def set_accel(self, target):
+        lower = target & 0x7f
+        upper = (target >> 7) & 0x7f
+        command = chr(0x89) + chr(self.channel) + chr(lower) + chr(upper)
+
+        maestro.write(bytes(command, 'latin-1'))
+
+    def min(self):
+        return self.min
+
+    def max(self):
+        return self.max
+
+
+def drive(drive_desu, steer_desu):
+    global previous_speed
+
+    steer.set_target(int(interp(steer_desu, [-100, 100], [steer.min, steer.max])))
+
+    if drive_desu < 0 and previous_speed > 0:
+        speed.set_target(5999)
+        speed.set_target(6001)
+        speed.set_target(int(interp(drive_desu, [-100, 100], [speed.min, speed.max])))
+    else:
+        speed.set_target(int(interp(drive_desu, [-100, 100], [speed.min, speed.max])))
+
+    previous_speed = int(interp(drive_desu, [-100, 100], [speed.min, speed.max]))
+
+    previous_speed = drive_desu
+
+
+def stop():
+    #speed.set_speed(50)
+    #speed.set_accel(10)
+    speed.set_target(6000)
+    #time.sleep(1)
+    #speed.set_speed(0)
+    #speed.set_accel(0)
+
+
+if __name__ == "__main__":
+
+    steer = Channel(0)
+    speed = Channel(1)
+
+    steer.set_range(3000, 9000)
+    speed.set_range(3000, 9000)
+
+    #steer.set_target(7000)
+    #speed.set_target(5000)
+    steer.set_speed(10)
+    steer.set_accel(10)
+
+    drive(20, 40)
+    time.sleep(2)
+    stop()
---
+++ Modules/test-maestro.py
@@ -0,0 +1,40 @@
+import serial
+
+steer_channel = 0
+drive_channel = 1
+
+steer_min = 1000
+steer_max = 11000
+drive_min = 1000
+drive_max = 11000
+
+maestro = serial.Serial(port='COM17')
+
+
+def close_maestro():
+    maestro.close()
+
+
+def set_target(channel, target):
+    if channel == steer_channel:
+        if target < steer_min:
+            target = steer_min
+        if target > steer_max:
+            target = steer_max
+    if channel == drive_channel:
+        if target < drive_min:
+            target = drive_min
+        if target > drive_max:
+            target = drive_max
+
+    lower = target & 0x7f
+    upper = (target >> 7) & 0x7f
+    command = chr(0x84) + chr(channel) + chr(lower) + chr(upper)
+
+    maestro.write(bytes(command, 'latin-1'))
+
+
+set_target(drive_channel, 6000)
+set_target(steer_channel, 6000)
+
+
